c899c4430f6787ec81a38ba4d9c06505de6352a3
wave-rider <wave-rider@users.noreply.github.com>
2017-12-04 22:15:39 +1300

    Added setup script

---
+++ Setup.ps1
@@ -0,0 +1,27 @@
+﻿Add-Type -assembly "system.io.compression.filesystem"
+$url = "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.bin.WIN64.zip"
+$output = "lib\glfw-3.2.1.bin.WIN64.zip"
+Write-Host $output
+$start_time = Get-Date
+if ((Test-Path "lib") -eq $false)
+{
+ New-Item -ItemType Directory -Force -Path 'lib'
+}
+
+if ((Test-Path $output) -eq $false)
+{ 
+  Invoke-WebRequest -Uri $url -OutFile $output
+}
+
+  [io.compression.zipfile]::ExtractToDirectory($output, "lib")
+
+$url = "https://github.com/g-truc/glm/releases/download/0.9.8.5/glm-0.9.8.5.zip"
+$output = "lib\glm-0.9.8.5.zip"
+if ((Test-Path $output) -eq $false)
+{ 
+  Invoke-WebRequest -Uri $url -OutFile $output
+}
+  [io.compression.zipfile]::ExtractToDirectory($output, "lib")
+
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
---
+++ setup.bat
@@ -0,0 +1,1 @@
+ Powershell.exe -ExecutionPolicy Bypass .\Setup.ps1
